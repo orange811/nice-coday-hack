a6df1e6443a90bdd3646f1aaeb675efdd7218d4d
ncpshetti1d <ncpshetti1d@gmail.com>
2024-09-14 16:28:22 +0530

    charging station

--- HackHarmonics_Java/src/main/java/com/nice/coday/ElectricityConsumptionCalculatorImpl.java
+++ HackHarmonics_Java/src/main/java/com/nice/coday/ElectricityConsumptionCalculatorImpl.java
@@ -3,8 +3,6 @@ package com.nice.coday;
 import java.io.IOException;
 import java.util.Map;
 
-import org.apache.poi.ss.formula.eval.NotImplementedException;
-
 public class ElectricityConsumptionCalculatorImpl implements ElectricityConsumptionCalculator {
 
     @Override
@@ -13,10 +11,57 @@ public class ElectricityConsumptionCalculatorImpl implements ElectricityConsumpt
         AllDetails allDetails = new AllDetails(resourceInfo);
         allDetails.displayAllDetails();
 
+<<<<<<< Updated upstream
         throw new NotImplementedException("Not implemented yet.");
     }
 
     TripDetails calculateTripDetails(AllDetails allDetails, int tripId) {
+=======
+        for (int tripId : allDetails.tripDetails.keySet()) {
+            TripDetails tripDetails = calculateTripDetails(allDetails, tripId, consumptionResult);
+            //printTripDetails(tripId, tripDetails);
+
+            if (tripDetails.finished) {
+                // Get or create the AggregatedResults for the current vehicle type
+                AggregatedResults results = vehicleDetails.computeIfAbsent(tripDetails.vehicleType, k -> new AggregatedResults());
+                
+
+                // Update aggregated results
+                results.totalUnitsConsumed += tripDetails.unitConsumed;
+                results.totalTimeRequired += tripDetails.timeRequired;
+                results.numberOfTripsFinished++;
+            }
+        }
+        printAggregatedResults(vehicleDetails);
+        return consumptionResult;
+        // throw new NotImplementedException("Not implemented yet.");
+    }
+
+    private void printAggregatedResults(Map<Integer, AggregatedResults> vehicleTypeAggregates) {
+        System.out.println("Aggregated Results by Vehicle Type:");
+        for (Map.Entry<Integer, AggregatedResults> entry : vehicleTypeAggregates.entrySet()) {
+            int vehicleType = entry.getKey();
+            AggregatedResults results = entry.getValue();
+            System.out.println("Vehicle Type: V" + vehicleType);
+            System.out.println("Total Units Consumed: " + results.totalUnitsConsumed);
+            System.out.println("Total Time Required: " + results.totalTimeRequired);
+            System.out.println("Number of Trips Finished: " + results.numberOfTripsFinished);
+            System.out.println(); // Print a blank line for better readability
+        }
+    }
+
+    private void printTripDetails(int tripId, TripDetails tripDetails) {
+        System.out.println("Trip ID: " + tripId);
+        System.out.println("Vehicle Type: V" + tripDetails.vehicleType);
+        System.out.println("Units Consumed: " + tripDetails.unitConsumed);
+        System.out.println("Time Required: " + tripDetails.timeRequired);
+        System.out.println("Finished: " + (tripDetails.finished ? "Yes" : "No"));
+        System.out.println(); // Print a blank line for better readability
+    }
+
+    TripDetails calculateTripDetails(AllDetails allDetails, int tripId, ConsumptionResult consumptionResult) {
+
+>>>>>>> Stashed changes
         TripDetails tripDetails = new TripDetails();
         int vehicleType = allDetails.tripDetails.get(tripId)[1];
         int fullChargeDist = allDetails.vehicleTypeInfo.get(vehicleType)[2];
@@ -25,7 +70,7 @@ public class ElectricityConsumptionCalculatorImpl implements ElectricityConsumpt
         int entryPointDist = allDetails.entryExitPointInfo.get(allDetails.tripDetails.get(tripId)[3]);
         int exitPointDist = allDetails.entryExitPointInfo.get(allDetails.tripDetails.get(tripId)[4]);
         int distRequired = exitPointDist - entryPointDist;
-
+        
         //no charging needed
         if (initDistRemaining <= distRequired) {
             tripDetails.vehicleType = vehicleType;
@@ -65,9 +110,16 @@ public class ElectricityConsumptionCalculatorImpl implements ElectricityConsumpt
             }
             int distTravelledThisIteration = currentStationDist - latestStationDist;
             double chargeUnitsConsumedThisIteration = 1.0 * distTravelledThisIteration / fullChargeDist * fullChargeUnit;
+            long timeRequiredThisIteration = (long) (chargeUnitsConsumedThisIteration * allDetails.timeToChargeVehicleInfo.get(new VCSPair(vehicleType, allDetails.chargingStationInfo.get(currentStationDist))));
             tripDetails.unitConsumed += chargeUnitsConsumedThisIteration;
+<<<<<<< Updated upstream
             tripDetails.timeRequired += allDetails.timeToChargeVehicleInfo.get(new VCSPair(vehicleType, allDetails.chargingStationInfo.get(currentStationDist))) * chargeUnitsConsumedThisIteration;
+=======
+            tripDetails.timeRequired += timeRequiredThisIteration;
+>>>>>>> Stashed changes
             distTravelled += distTravelledThisIteration;
+            String chargingStation= "C"+allDetails.chargingStationInfo.get(currentStationDist);
+            consumptionResult.totalChargingStationTime.put(chargingStation,consumptionResult.totalChargingStationTime.getOrDefault(chargingStation,0l)+timeRequiredThisIteration);
             latestStationDist = currentStationDist;
         }
         tripDetails.vehicleType = vehicleType;
